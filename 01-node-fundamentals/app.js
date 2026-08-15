492ea517d75fdf5fa0c87565a8c81c7124cf3f75
REDRAGON <148379059+REDRAGO14@users.noreply.github.com>
2026-08-15 00:28:56 -0700

    learn node js internal

---
+++ 01-node-fundamentals/app.js
@@ -0,0 +1,3 @@
+/* learned basics about what node js is. it is single threaded javascript exution enviroment built on top of v8 engin.
+    saw it internal structure. so node js is a bundle of library with javascript and the main problem it solve is running javascript program out side browser so it consists javascript, libuv, crypto , file io , os and more which lift problems that javascript can't handle it own*/
+console.log(process);
