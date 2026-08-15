4e4de5d448f315f55bf4db00d462226bc0781f1d
REDRAGON <148379059+REDRAGO14@users.noreply.github.com>
2026-08-15 03:54:06 -0700

    Nginx sever: complete Nginx sever by my self

--- 03-http-server/server.js
+++ 03-http-server/server.js
@@ -4,7 +4,28 @@ const path = require('path')
 
 const port = 3000
 
-const server = http.createServer((res, req) =>{
+const server = http.createServer((req, res) =>{
+    const filePath = path.join(__dirname, req.url === "/"|| req.url === "/home" ? "index.html": req.url)
+    
+    const exName = String(path.extname(filePath).toLowerCase())
+    const mimeType = {
+        ".html": 'text/html',
+        ".css": 'text/css',
+        ".js": 'text/javascript',
+        ".png": 'text/png'
+    }
+    const contentType = mimeType[exName] || "application/octet-stream"
+
+    fs.readFile(filePath, (err, content) =>{
+        if(err){
+            if(err.code === "ENOENT"){ res.writeHead(404, {"Content-Type": "text/html"})
+                 res.end("404 page not found")
+            }
+            }else{
+                res.writeHead(200, {"Content-Type": contentType})
+                res.end(content, "utf-8")
+            }
+    })
 })
 
 server.listen(port, ()=>{
