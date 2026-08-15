02d32da7e3ecf54a97cdb6554e5f8d7703526715
REDRAGON <148379059+REDRAGO14@users.noreply.github.com>
2026-08-15 00:53:12 -0700

    accept argument from cli in node

---
+++ 02-files/todo.js
@@ -0,0 +1,15 @@
+const fs = require("fs")
+const filePath = "./tasks.json"
+
+const command = process.argv[2]
+const argument = process.argv[3]
+
+if(command == "add"){
+    addTask(argument)
+}else if(command == "remove"){
+    removeTask(parseInt(argument))
+}else if(command == list){
+    listTasks()
+}else{
+    console.log("command not found");
+}
