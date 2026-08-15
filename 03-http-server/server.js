06a57749f48bd291a89c6c7b5101123335f8921e
REDRAGON <148379059+REDRAGO14@users.noreply.github.com>
2026-08-15 03:18:42 -0700

    import modules , create create and start  listening to req

--- 03-http-server/server.js
+++ 03-http-server/server.js
@@ -4,34 +4,7 @@ const path = require('path')
 
 const port = 3000
 
-const server = http.createServer((req, res) => {
-    console.log(req.url);
-    
-    const filePath = path.join(__dirname, req.url === "/" ? "index.html" : `${req.url}`)
-    const exName = String(path.extname(filePath).toLowerCase())
-
-    const mimeType = {
-        ".html": "text/html",
-        ".css": "text/css",
-        ".js": "text/javascript",
-        ".png": "text/png",
-    }
-
-    const contentType = mimeType[exName] || "application/octet-stream"
-
-    fs.readFile(filePath, (err, content)=>{
-        if(err){
-            if(err.code === "ENOENT"){
-                console.log(err);
-                
-                res.writeHead(404, {"Content-type": 'text/html'})
-                res.end("404 page not found")
-            }
-        }else{
-            res.writeHead(200, {"Content-Type": contentType})
-            res.end(content)
-        }
-    })
+const server = http.createServer((res, req) =>{
 })
 
 server.listen(port, ()=>{
