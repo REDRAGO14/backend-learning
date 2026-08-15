aaee3d8612106dec83f4d8b2d2326dad7be1e1db
REDRAGON <148379059+REDRAGO14@users.noreply.github.com>
2026-08-15 01:24:57 -0700

    list feature added

--- 02-files/todo.js
+++ 02-files/todo.js
@@ -3,27 +3,39 @@ const filePath = "./tasks.json"
 
 const command = process.argv[2]
 const argument = process.argv[3]
+
 const loadTask = () =>{
     const dataBuffer = fs.readFileSync(filePath)
     const dataJSON = dataBuffer.toString()
     return JSON.parse(dataJSON)
 }
+
 const saveTask = (tasks) =>{
     const dataJSON = JSON.stringify(tasks)
     fs.writeFileSync(filePath, dataJSON)
 }
+
 const addTask = (task) => {
     const tasks = loadTask()
-    console.log(tasks);
     tasks.push(task)
     saveTask(tasks)
+    console.log("Task Added Successfully");
+    
+}
+
+const listTasks = () =>{
+    const tasks = loadTask()
+    tasks.forEach((task,index) => {
+        console.log(`[${index + 1}] ${task}`);
+    });
+    
 }
 
 if(command == "add"){
     addTask(argument)
 }else if(command == "remove"){
     removeTask(parseInt(argument))
-}else if(command == list){
+}else if(command == "list"){
     listTasks()
 }else{
     console.log("command not found");
