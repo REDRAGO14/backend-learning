80b149a53f2a7dedeb12d2fe08342ad18f21484f
REDRAGON <148379059+REDRAGO14@users.noreply.github.com>
2026-08-15 01:09:17 -0700

    implement addTask() function

--- 02-files/todo.js
+++ 02-files/todo.js
@@ -4,6 +4,12 @@ const filePath = "./tasks.json"
 const command = process.argv[2]
 const argument = process.argv[3]
 
+const addTask = (task) => {
+    const tasks = loadTask()
+    tasks.push(task)
+    saveTask(tasks)
+}
+
 if(command == "add"){
     addTask(argument)
 }else if(command == "remove"){
