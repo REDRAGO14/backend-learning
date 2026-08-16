6bf170985a8b61c5229e9a3a84ad53bd577e2072
REDRAGON <148379059+REDRAGO14@users.noreply.github.com>
2026-08-16 14:35:03 -0700

    project structure done

---
+++ System_Monitor_Server/server.js
@@ -0,0 +1,12 @@
+const http = require('http')
+const path = require('path')
+const fs = require('fs')
+
+const port = 3000
+
+const server = http.createServer((req, res) =>{
+    
+    
+})
+
+server.listen(port, ()=> console.log("server is running"))
